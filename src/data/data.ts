78182f574b874d0a7438ca3fb419fd6716ebb32e
pablo <goncalves.pab@gmail.com>
2023-07-02 15:07:57 -0300

    update

--- src/data/data.ts
+++ src/data/data.ts
@@ -139,7 +139,7 @@ export const Me = {
     {
       name: 'Universidad Tecnologica Nacional',
       start: '2019',
-      end: 'en curso',
+      end: 'Curso',
       title: 'Ingenieria en sistemas de informacion'
     }
   ]
